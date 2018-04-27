2d0fc00538dc529a4ed05087919a204309f27e03
IWANABETHATGUY <974153916@qq.com>
2018-04-27 19:46:54 +0800

    feat: finish

---
+++ san-helloworld/build/webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'main': path.join(__dirname, '../src/index.js')
+  },
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.san$/,
+        use: {
+          loader: 'san-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  // resolve: {
+  //   alias: {
+  //     san: process.env.NODE_ENV === 'production' ?
+  //       'san/dist/san.js' :
+  //       'san/dist/san.dev.js'
+  //   }
+  // },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, '../template/index.html')
+    })
+  ]
+}
